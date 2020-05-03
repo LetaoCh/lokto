17d1a8eedd0b94ead635cd70ddf086c0affeda4d
Letao Chen <itsumo0628@gmail.com>
2020-05-02 21:50:56 -0400

    added bootstrap

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -9,18 +9,14 @@ export default class Intro extends Component {
           href="https://drive.google.com/file/d/1DJQPZrm4v3E9V_TOQB0ixAloCXzpRV5O/view?usp=sharing"
           target="_blank"
           rel="noopener noreferrer">
-          View Resume
-          <span class="fa-stack">
-            <i class="fa fa-download" aria-hidden="true"></i>
-          </span>
+          View Resume <i class="fa fa-download" aria-hidden="true"></i>
         </a>
         </p>
-        <h1>I am Currently 
-          <br />Working on This Page. 
-          <br />It'll Be Awesome 
-          <br />if U Can
-          <br />Come Back
-          <br />& Check it Out Later!
+        <h1>I am currently 
+          <br />working on the page. 
+          <br />It'll be awesome 
+          <br />if you can come back
+          <br />& check it out later!
         </h1>
       </div>
     )
