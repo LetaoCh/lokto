43ee8c64a7a6c41f05285ce1e826b99af44f8679
Letao Chen <itsumo0628@gmail.com>
2020-05-02 20:46:01 -0400

    Updates

---
+++ precache-manifest.fde919eaa43a74406dccc6f66ee1906f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58366cd8036ad7bd4548c3ec6d771f9b",
+    "url": "/lokto/index.html"
+  },
+  {
+    "revision": "a3cae1267ca28f184b2f",
+    "url": "/lokto/static/css/main.d3e480bd.chunk.css"
+  },
+  {
+    "revision": "8b7ada0060c25bd59c5f",
+    "url": "/lokto/static/js/2.fef9fd1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lokto/static/js/2.fef9fd1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3cae1267ca28f184b2f",
+    "url": "/lokto/static/js/main.b3d51228.chunk.js"
+  },
+  {
+    "revision": "bfcc526488753a4a51eb",
+    "url": "/lokto/static/js/runtime-main.a45a64a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lokto/static/media/logo.5d5d9eef.svg"
+  }
+]);
