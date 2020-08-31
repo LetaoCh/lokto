676258eb406aa52afe4985853eb82717e5e58346
Letao Chen <letaochen@Letaos-iMac.local>
2020-08-31 18:59:29 -0400

    update page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -116,7 +116,7 @@ export default function HomePage() {
           <br />
           <Button
             variant="outline-dark"
-            href="https://drive.google.com/file/d/1DJQPZrm4v3E9V_TOQB0ixAloCXzpRV5O/view?usp=sharing"
+            href="https://drive.google.com/file/d/1Grr9UBIKYYcfTEhrdQ37n23G8SbZSZO0/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer">
             View Resume <i className="fa fa-download" aria-hidden="true"></i>
